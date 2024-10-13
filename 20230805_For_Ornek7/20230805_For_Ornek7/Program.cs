2a87cfff97f07f27475d0c6d56cc068b1029b043
sude <sudefidan@icloud.com>
2024-10-13 17:53:40 +0300

    for-ornek7

---
+++ 20230805_For_Ornek7/20230805_For_Ornek7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230805_For_Ornek7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ornek 7
+            //Çarpım Tablosu oluşturalım
+
+            for (int sayi1 = 1; sayi1 <= 10; sayi1++)
+            {
+                Console.WriteLine(sayi1 + " Çarpım Tablosu");
+                Console.WriteLine("------------------");
+
+                for (int sayi2 = 1; sayi2 <= 10; sayi2++)
+                {
+                    int carpimSonucu = sayi1 * sayi2;
+                    Console.WriteLine("{0} X {1} = {2}", sayi1, sayi2, carpimSonucu);
+                    //Console.WriteLine(sayi1 + " X "+ sayi2+ " = "+carpimSonucu);
+                }
+
+                Console.WriteLine(" ------------------- ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
